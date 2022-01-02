689ff29185bd4fc23ecdc24448d07a7b9b33a9d4
Nikita Guzhva <elit3guzhva@Nikitas-MacBook-Air.local>
2022-01-02 14:51:41 +0300

    [upd] Day 1, Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use curl::easy::{Easy, List};
+
 fn main() {
-    println!("Hello, world!");
+    let mut headers = List::new();
+    headers.append("Cookie: session=53616c7465645f5f598d394b71e013a53dffdcaf219d3b0a5703e40be38845f15204acf20f0b9c9d6d81807cecfb54fb").unwrap();
+
+    let mut easy = Easy::new();
+    easy.url("https://adventofcode.com/2021/day/1/input").unwrap();
+    easy.http_headers(headers).unwrap();
+
+    let mut sweep = Vec::<i32>::new();
+
+    let mut transfer = easy.transfer();
+    transfer.write_function(|data| {
+        let data_str = String::from_utf8(data.to_vec()).unwrap();
+        sweep = data_str.split('\n').into_iter()
+            .filter_map(|v| v.parse::<i32>().ok())
+            .collect();
+        Ok(data.len())
+    }).unwrap();
+    transfer.perform().unwrap();
+    drop(transfer);
+
+    let mut counter = 0;
+    let mut prev = sweep[0];
+    for value in sweep {
+        if value > prev {
+            counter += 1;
+        }
+        prev = value;
+    }
+
+    println!("Answer: {}", counter);
 }
