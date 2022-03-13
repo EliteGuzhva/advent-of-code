11c8f77fd9f55bc60f9e6a8146e736deb6211ebf
elit3guzhva <nik_finger@mail.ru>
2022-03-13 19:39:24 +0300

    [new] Day 3, Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -11,11 +11,39 @@ pub fn solve(data: &mut String) {
     });
 
     let gamma: u32 = sums
-        .into_iter()
-        .map(|d| if d > half { 1 } else { 0 })
+        .iter()
+        .map(|d| if d > &half { 1 } else { 0 })
         .fold(0, |res, digit| (res << 1) + digit);
 
     let epsilon = !gamma << 32 - number_length >> 32 - number_length;
     let res = gamma * epsilon;
     println!("Part 1: {}", res);
+
+    let filter_numbers = |f: fn(u32, u32) -> bool| {
+        let mut values = numbers.clone();
+        for i in 0..number_length {
+            let current_bit_values: Vec<u32> = values.iter().map(|digits| digits[i]).collect();
+            let current_half = (current_bit_values.len() as u32 + 1) / 2;
+            let bit_sum: u32 = current_bit_values.iter().sum();
+            let most_common = if bit_sum >= current_half { 1 } else { 0 };
+
+            values = values
+                .into_iter()
+                .filter(|value| f(value[i], most_common))
+                .collect();
+
+            if values.len() == 1 {
+                break;
+            }
+        }
+
+        values[0].iter().fold(0, |res, digit| (res << 1) + digit)
+    };
+
+    let oxygen_res = filter_numbers(|d, c| d == c);
+    let co2_res = filter_numbers(|d, c| d != c);
+
+    let res = oxygen_res * co2_res;
+
+    println!("Part 2: {}", res);
 }
